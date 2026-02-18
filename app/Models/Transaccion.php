c04293dcfad79d5c17c33464c37cde9f3393722a
AdrianAlonso23 <adrianruad@gmail.com>
2026-02-18 17:40:50 +0100

    cambio de nombre de los tablas en los modelos

--- app/Models/Transaccion.php
+++ app/Models/Transaccion.php
@@ -1,24 +1,16 @@
 <?php
 
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+namespace app\Models;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Transaccion extends Model
-{
+class Transaccion extends Model{
     use HasFactory;
 
     protected $table = 'transacciones';
+    protected $fillable = ['user_id', 'tipo', 'cantidad'];
 
-    protected $fillable = [
-        'user_id',
-        'type',
-        'amount',
-    ];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
+    public function user(){
+        return $this->BelongTo(User::class);
     }
 }
